e4ca17d8435de4e6e04aa23c5d2e14c9a160c2d0
andregn26 <andregn26@gmail.com>
2023-12-26 17:09:21 +0000

    lab done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Movie = require("../models/Movie.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res, next) => {
+	Movie.find().then((allMoviesFromDB) => {
+		// console.log({ movies: allMoviesFromDB });
+		res.render("movies", { movies: allMoviesFromDB });
+	});
+});
+
+router.get("/movies/:id", (req, res) => {
+	const { id } = req.params;
+	console.log(id);
+	Movie.findById(id).then((movieById) => {
+		console.log(movieById);
+		res.render("movie", movieById);
+	});
+});
 
 module.exports = router;
